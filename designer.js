86482c130e6e05359613ca127497b08857da211c
Stéphane Puybareau <stephane.puybareau@4d.com>
2014-10-07 15:59:21 +0200

    fixing many bugs

--- designer.js
+++ designer.js
@@ -2,4 +2,59 @@
     "use strict";
 
     MatrixReloaded.containerChildrenAreSubWidgets();
+
+    var updateSize = function() {
+        var coords = this.repeaterGetCoordinates(0);
+        console.log(coords);
+        console.trace();
+        for(var key in coords) {
+            this.widget(0)[key](coords[key]);
+        }
+        var num = this._getColumnCount();
+        if(this.isHorizontalScroll()) {
+            num *= Math.ceil(this.width() / this.getRowSize());
+        } else {
+            num *= Math.ceil(this.height() / this.getRowSize());
+        }
+        num--;
+        this.studioRepeatedClones(num);
+    };
+
+    var Container = WAF.require('Container');
+    MatrixReloaded.prototype.studioRepeaterGetClone = function(position) {
+        var widget = new Container();
+        widget.node.innerHTML = position + 1;
+        return widget;
+    };
+
+    MatrixReloaded.doAfter('_init', function() {
+        this.direction.onChange(updateSize);
+        this.number.onChange(updateSize);
+        this.columnSize.onChange(updateSize);
+        this.rowSize.onChange(updateSize);
+        this.expand.onChange(updateSize);
+        this.rowMargin.onChange(updateSize);
+        this.columnMargin.onChange(updateSize);
+        updateSize.call(this);
+
+        var showMode = function() {
+            if(this.mode() === 'split') {
+                this.number.show();
+                this.columnSize.hide();
+                this.expand.hide();
+            } else {
+                this.number.hide();
+                this.columnSize.show();
+                this.expand.show();
+            }
+            updateSize.call(this);
+        };
+        this.mode.onChange(showMode);
+        showMode.call(this);
+    });
+
+    MatrixReloaded.studioOnResize(function() {
+        //debugger;
+        updateSize.call(this);
+    });
 });
--- widget.js
+++ widget.js
@@ -2,10 +2,31 @@ WAF.define('MatrixReloaded', ['waf-core/widget', 'Container'], function(widget, 
     "use strict";
 
     var MatrixReloaded = widget.create('MatrixReloaded', {
-        collection: widget.property({ type: 'datasource' })
+        collection: widget.property({ type: 'datasource', pageSize: 40 }),
+        direction: widget.property({
+            type: 'enum',
+            values: {
+                'vertical':   'vertical',
+                'horizontal': 'horizontal'
+            }
+        }),
+        mode: widget.property({
+            type: 'enum',
+            values: {
+                'split': 'number of columns',
+                'size': 'size'
+            }
+        }),
+        number:       widget.property({ type: 'integer', defaultValue: 2 }),
+        columnSize:   widget.property({ type: 'number',  defaultValue: 150 }),
+        columnMargin: widget.property({ type: 'number',  defaultValue: 10 }),
+        rowSize:      widget.property({ type: 'number',  defaultValue: 150 }),
+        rowMargin:    widget.property({ type: 'number',  defaultValue: 10 }),
+        expand:       widget.property({ type: 'boolean', defaultValue: true })
     });
+    var proto = MatrixReloaded.prototype;
 
-    MatrixReloaded.inherit('waf-behavior/layout/repeater');
+    MatrixReloaded.inherit('waf-behavior/layout/repeater-livescroll');
     MatrixReloaded.linkDatasourcePropertyToRepeater('collection');
     MatrixReloaded.repeatedWidget(Container);
 
@@ -18,13 +39,13 @@ WAF.define('MatrixReloaded', ['waf-core/widget', 'Container'], function(widget, 
             return [];
         }
 
-        var r = getSelectorRegExp(id);
+        var regexp = getSelectorRegExp(id);
         var res = [];
         for(var i = 0; i < document.styleSheets.length; i++) {
             var rules = document.styleSheets[i].cssRules ||
                         document.styleSheets[i].rules;
             for(var j = 0; j < rules.length; j++) {
-                if(r.test(rules[j].selectorText)) {
+                if(regexp.test(rules[j].selectorText)) {
                     res.push(rules[j]);
                 }
             }
@@ -33,12 +54,12 @@ WAF.define('MatrixReloaded', ['waf-core/widget', 'Container'], function(widget, 
     }
 
     function upgradeRules(id, newClass) {
-        var r = getSelectorRegExp(id);
+        var regexp = getSelectorRegExp(id);
         getRules(id).forEach(function(rule) {
             var selectors = rule.selectorText.split(/ *, */);
             selectors.some(function(selector) {
-                if(r.test(selector)) {
-                    selectors.push(selector.replace(r, '.' + newClass));
+                if(regexp.test(selector)) {
+                    selectors.push(selector.replace(regexp, '.' + newClass));
                     return true;
                 }
             });
@@ -49,25 +70,85 @@ WAF.define('MatrixReloaded', ['waf-core/widget', 'Container'], function(widget, 
     function upgradeWidgetAndRules(widget) {
         var newClass = 'waf-clone-' + widget.node.id;
         upgradeRules(widget.node.id, newClass);
-        widget.addClass(newClass);  
+        widget.addClass(newClass);
     }
 
-    MatrixReloaded.prototype.init = function() {
+    proto.isHorizontalScroll = function() {
+        return this.direction() === 'horizontal';
+    };
+
+    proto._getColumnsAvailableSize = function() {
+        if(this.isHorizontalScroll()) {
+            return this.node.clientHeight + this.columnMargin();
+        }
+        return this.node.clientWidth + this.columnMargin();
+    };
+
+    proto._getColumnSize = function() {
+        if(this.mode() === 'split') {
+            return (this._getColumnsAvailableSize() - this.number() * this.columnMargin()) / this.number();
+        }
+        if(this.expand()) {
+            return this._getColumnsAvailableSize() / Math.floor(this._getColumnsAvailableSize() / (this.columnSize() + this.columnMargin())) - this.columnMargin();
+        }
+        return this.columnSize();
+    };
+
+    proto._getColumnCount = function() {
+        return Math.floor(this._getColumnsAvailableSize() / (this._getColumnSize() + this.columnMargin()));
+    };
+
+    proto._getTop = function(position) {
+        var row = Math.floor(position / this._getColumnCount());
+        return (this.rowSize() + this.rowMargin()) * row;
+    };
+
+    proto._getLeft = function(position) {
+        var column = position % this._getColumnCount();
+        return (this._getColumnSize() + this.columnMargin()) * column;
+    };
+
+    proto.repeaterGetCoordinates = function(position) {
+        var res = {};
+        if(!this.isHorizontalScroll()) {
+            res.left   = this._getLeft(position);
+            res.top    = this._getTop(position);
+            res.width  = this._getColumnSize();
+            res.height = this.rowSize();
+        } else {
+            res.top    = this._getLeft(position);
+            res.left   = this._getTop(position);
+            res.height = this._getColumnSize();
+            res.width  = this.rowSize();
+        }
+        //console.log(JSON.stringify(res));
+        return res;
+    };
+
+    proto.getScrolledNode  = function() {
+        if(!$('>div', this.node).length) {
+            this.node.innerHTML = '<div></div>';
+        }
+        return $('>div', this.node).get(0);
+    };
+
+
+    // Live scroll method
+    proto.getRowSize = function() {
+        return this.rowSize() + this.rowMargin();
+    };
+
+    // Live scroll method
+    proto.getItemsPerRow = function() {
+        return this._getColumnCount();
+    };
+
+    proto.init = function() {
         // get master css and transform it in classes
-        if(this.repeatedWidget()) {
+        if(!window.Designer && this.repeatedWidget()) {
             upgradeWidgetAndRules(this.repeatedWidget());
             this.repeatedWidget().allChildren().forEach(upgradeWidgetAndRules);
         }
-        // find how many per page
-
-        // install scroll callback handler
-
-    };
-
-    MatrixReloaded.prototype.getNewItem = function(position) {
-        var w = this.$super('getNewItem')(position);
-        w.node.style.position = 'relative';
-        return w;
     };
 
 
